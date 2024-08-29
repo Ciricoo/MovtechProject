19671996f45a55e5ce0fb5007321561b4039bd50
Estagiario 3 <estagiario3@movtech.com.br>
2024-08-29 17:33:40 -0300

    feat: implementation of the nps-graph

--- src/app/pages/nps/components/nps-graph/nps-graph.component.ts
+++ src/app/pages/nps/components/nps-graph/nps-graph.component.ts
@@ -1,74 +1,34 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component, OnInit } from '@angular/core';
+import { NpsService } from 'src/app/services/nps/nps.service';
 
 @Component({
   selector: 'app-nps-graph',
   templateUrl: './nps-graph.component.html',
   styleUrls: ['./nps-graph.component.scss']
 })
-export class NpsGraphComponent implements AfterViewInit {
+export class NpsGraphComponent implements OnInit {
+  npsScore!: number;
 
-  ngAfterViewInit(): void {
-    this.createMainChart();
-    this.createSideCharts();
-  }
+  constructor(private npsService: NpsService) {}
 
-  createMainChart() {
-    const ctx = (document.getElementById('mainChart') as HTMLCanvasElement).getContext('2d');
-    if (ctx) {
-      new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-          labels: ['Promoters', 'Passives', 'Detractors'],
-          datasets: [{
-            data: [50, 30, 20],
-            backgroundColor: ['#4caf50', '#ff9800', '#f44336'],
-            borderWidth: 0,
-          }]
-        },
-        options: {
-          rotation: -90,
-          circumference: 180,
-          cutout: '70%',
-          plugins: {
-            legend: {
-              display: false
-            }
-          }
-        }
-      });
-    }
-  }
-
-  createSideCharts() {
-    const sideChartsData = [80, 50];
-    const sideChartsColors = ['#4caf50', '#f44336'];
-
-    ['leftChart', 'rightChart'].forEach((id, index) => {
-      const ctx = (document.getElementById(id) as HTMLCanvasElement).getContext('2d');
-      if (ctx) {
-        new Chart(ctx, {
-          type: 'doughnut',
-          data: {
-            labels: ['Data'],
-            datasets: [{
-              data: [sideChartsData[index], 100 - sideChartsData[index]],
-              backgroundColor: [sideChartsColors[index], '#e0e0e0'],
-              borderWidth: 0,
-            }]
-          },
-          options: {
-            rotation: -90,
-            circumference: 180,
-            cutout: '70%',
-            plugins: {
-              legend: {
-                display: false
-              }
-            }
-          }
-        });
-      }
+  ngOnInit(): void {
+    this.npsService.getNpsScore().subscribe(score => {
+      this.npsScore = score;
+      this.updateNeedlePosition(score);
     });
+  }
+
+  // RESPOSTA NÃO É ENVIADA QUANDO SELECIONADA "0"!!!!!!!!!
+
+  updateNeedlePosition(score: number): void {
+    const minScore = -100;
+    const maxScore = 100;
+    
+    const angle = ((score - minScore) / (maxScore - minScore)) * 180 - 90; 
+
+    const needleImg = document.querySelector('.needle-img') as HTMLElement;
+    if (needleImg) {
+      needleImg.style.transform = `rotate(${angle + 180}deg)`; 
+    }
   }
 }
---
+++ src/app/services/nps/nps.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NpsService {
+  private apiUrl = 'https://localhost:7193/api/Nps';
+  
+  constructor(private http: HttpClient) {}
+
+  getNpsScore(): Observable<number> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    return this.http.get<number>(this.apiUrl, { headers });
+  }
+}
